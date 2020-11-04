9b6e006fa9010160b5648000d79d180c8c8ad247
William Damm <wdamm@powerhrg.com>
2020-11-04 18:17:55 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,3 +7,14 @@ document.body.appendChild(element);
 
 element.style.textAlign = 'center';
 
+var ul = document.createElement('ul');
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+element.appendChild(ul);
+
+
